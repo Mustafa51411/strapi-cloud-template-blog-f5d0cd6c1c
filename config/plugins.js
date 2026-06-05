8b73e6668f9543210de85d27059162b1f462691e
Mustafa51411 <122411494+Mustafa51411@users.noreply.github.com>
2026-06-05 15:45:02 +0300

    Disable cloud cronjob runner

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  'cloud-cronjob-runner': {
+    enabled: false,
+    config: {
+      apiToken: env('CLOUD_CRONJOB_API_TOKEN', 'disabled'),
+      apiUrl: env('CLOUD_CRONJOB_API_URL', 'https://example.com'),
+      firstRunWindow: env.int('CLOUD_CRONJOB_FIRST_RUN_WINDOW', 1),
+    },
+  },
+});
