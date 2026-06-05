5291a2e07ab47bd23d43e2f2fbdb6e36c5870d65
Mustafa51411 <122411494+Mustafa51411@users.noreply.github.com>
2026-06-05 16:34:13 +0300

    Force cron runner runtime config

--- scripts/start.js
+++ scripts/start.js
@@ -1,3 +1,5 @@
+'use strict';
+
 process.env.API_TOKEN = process.env.API_TOKEN || process.env.CLOUD_CRONJOB_API_TOKEN || 'disabled';
 process.env.API_URL =
   process.env.API_URL ||
@@ -6,13 +8,4 @@ process.env.API_URL =
   'https://hopeful-duck-1d6f1130fb.strapiapp.com';
 process.env.FIRST_RUN_WINDOW = process.env.FIRST_RUN_WINDOW || process.env.CLOUD_CRONJOB_FIRST_RUN_WINDOW || '1';
 
-module.exports = () => ({
-  'cloud-cronjob-runner': {
-    enabled: true,
-    config: {
-      apiToken: process.env.API_TOKEN,
-      apiUrl: process.env.API_URL,
-      firstRunWindow: Number(process.env.FIRST_RUN_WINDOW),
-    },
-  },
-});
+require('@strapi/strapi/bin/strapi');
