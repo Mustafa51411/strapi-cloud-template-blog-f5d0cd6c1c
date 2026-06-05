e012f57ace6f723b7c6f6f7009216373854ca9fa
Mustafa51411 <122411494+Mustafa51411@users.noreply.github.com>
2026-06-05 16:19:17 +0300

    Set cron runner defaults on start

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,1 @@
-module.exports = ({ env }) => ({
-  'cloud-cronjob-runner': {
-    enabled: false,
-    config: {
-      apiToken: env('CLOUD_CRONJOB_API_TOKEN', 'disabled'),
-      apiUrl: env('CLOUD_CRONJOB_API_URL', 'https://example.com'),
-      firstRunWindow: env.int('CLOUD_CRONJOB_FIRST_RUN_WINDOW', 1),
-    },
-  },
-});
+module.exports = () => ({});
